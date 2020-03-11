0be69fca4bbdd19526e5210c4e15d8eb85e2615e
Anna <anyka69@mail.ru>
2020-03-11 22:30:44 +0300

    Test CheckBox

--- src/test/java/pages/Herokuapp.java
+++ src/test/java/pages/Herokuapp.java
@@ -13,18 +13,47 @@ public class Herokuapp extends BasePage {
     public Herokuapp(WebDriver driver) {
         super(driver);
     }
-    public void openPage(String url){
+
+    public void openPage(String url) {
         driver.get(url);
     }
-    public void rightClickTest(){
-        Actions actions= new Actions(driver);
-        WebElement contextMenu=driver.findElement(By.id("hot-spot"));
+
+    public void rightClickTest() {
+        Actions actions = new Actions(driver);
+        WebElement contextMenu = driver.findElement(By.id("hot-spot"));
         actions.contextClick(contextMenu).perform();
     }
-    public void validationTextAndClose(){
+
+    public void validationTextAndClose() {
         Alert alert = driver.switchTo().alert();
-        String element=driver.switchTo().alert().getText();
-        assertEquals(element,"You selected a context menu","Error");
+        String element = driver.switchTo().alert().getText();
+        assertEquals(element, "You selected a context menu", "Error");
         alert.accept();
+    }
+
+    public void findCheckBox() {
+        driver.findElement(By.xpath("//*[@id=\"checkbox\"]/input")).click();
+        driver.findElement(By.xpath("//*[@id=\"checkbox-example\"]/button")).click();
+    }
+
+    public void verificationCheckBox() {
+        String message = driver.findElement(By.id("message")).getText();
+        assertEquals(message, "It's gone!", "Error");
+    }
+
+    public void writeText(String text) {
+        driver.findElement(By.xpath("//*[@id=\"input-example\"]/input")).sendKeys(text);
+    }
+
+    public void checkEnable() {
+        driver.findElement(By.xpath("//*[@id=\"input-example\"]/button")).click();
+        String enable = driver.findElement(By.id("message")).getText();
+        assertEquals(enable, "It's enabled!", "Error");
+    }
+
+    public void checkDisable() {
+        driver.findElement(By.xpath("//*[@id=\"input-example\"]/button")).click();
+        String disable = driver.findElement(By.id("message")).getText();
+        assertEquals(disable, "It's disabled!", "Error!");
     }
 }
